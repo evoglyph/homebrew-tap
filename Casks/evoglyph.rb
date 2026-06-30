92c49fc5ea88927d73875dbd61d27bd1805872b2
Lucas McComb <lucasmccomb@gmail.com>
2026-06-30 16:17:14 -0400

    evoglyph 0.2.7

--- Casks/evoglyph.rb
+++ Casks/evoglyph.rb
@@ -1,7 +1,7 @@
 cask "evoglyph" do
-  version "0.2.6"
+  version "0.2.7"
   # version + sha256 are bumped by the app's release pipeline (Scripts/release.sh).
-  sha256 "3c2f05f0d561888e518774c39b1cc6ada00a3430d519334bfa6d7eb4d630b415"
+  sha256 "58ed6c67be339cd75b2c677229e4213b54619fd82ad524cbad5eae2a98f65fbf"
 
   url "https://downloads.evoglyph.com/evoglyph-#{version}.zip"
   name "evoglyph"
